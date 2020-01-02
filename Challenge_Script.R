2c2e50a6058e17c7a224cd5c6fd158084d94af39
ElenaDupen <elena.dupen@gmail.com>
2020-01-02 15:58:36 -0500

    Elena worked on creating a color palette

--- Challenge_Script.R
+++ Challenge_Script.R
@@ -33,6 +33,14 @@ nwss2 <- nwss %>%
          |DOM_HABITA == "Upland mixed ashwood"|DOM_HABITA == "Upland oakwood"
          |DOM_HABITA == "Wet woodland"|DOM_HABITA == "Lowland mixed deciduous woodland")
 
+# I thought I should save the dataframe as a csv to make the color palette in problem 2 easier
+# But this code did not work, even when I 
+# I read this StackOverflow thread which led me to the following line of code, 
+# but it turned the dataframe into a huge list, rather than unlisting the data:
+# https://stackoverflow.com/questions/15434138/function-write-csv-returns-an-error
+# nwss2 <- sapply(nwss2, unlist)
+# write.csv(nwss2, file = "nwss2.csv", rownames = FALSE)
+
 # Filter the NNR dataset so it contains only certain areas, then rename some areas using mutate(recode())
 nnr2 <- nnr %>% 
   filter(NAME == "The Great Trossachs Forest"|NAME == "Glen Affric"|NAME == "Cairngorms"
@@ -41,7 +49,30 @@ nnr2 <- nnr %>%
   mutate(NAME = recode_factor(NAME, "Mar Lodge Estate" = "Cairngorms")) %>% 
   mutate(NAME = recode_factor(NAME, "Abernethy" = "Cairngorms"))
 
-# Create a map of each of the three areas
+# 2. Create a map of each of the three areas
+# How many priority habitat types are there?
+unique(nwss2$DOM_HABITA)
+# Only six priority habitats (so we will only need six colors in our color palette)
+
+# Create a color palette for the habitat types
+habitat_colors <- c('#d73027','#fc8d59','#fee090','#e0f3f8','#91bfdb','#4575b4')
+
+# The following line of code doesn't work because there are technically 35 levels
+# in the nwss2 dataframe even though only six are actually included
+# names(habitat_colors) <- levels(nwss2$DOM_HABITA)
+
+# Let's make another color palette with 35 colors (colors by ColorBrewer):
+habitat_colors2 <- c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
+                      '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928',
+                     '#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462',
+                      '#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f',
+                     '#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf',
+                      '#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2')
+
+# Now the following line of code works, associating the colors with individual habitat types
+names(habitat_colors2) <- levels(nwss$DOM_HABITA)
+
+
 # This did not work (I need package after package, which isn't efficient with my slow internet)
 # scotmap <- gmap("Scotland", type = "satellite", exp = 3)
 # plot(scotmap)
