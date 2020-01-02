cc2f6153cfea4b2ec42a770757d4bcc4c2e5e361
ElenaDupen <elena.dupen@gmail.com>
2020-01-01 23:37:49 -0500

    Elena worked on problems 2 and 3

--- Challenge_Script.R
+++ Challenge_Script.R
@@ -4,10 +4,18 @@
 
 ### Load libraries ----
 library(tidyverse)
+library(sp)  # classes for spatial data
+library(raster)  # grids, rasters
+library(rasterVis)  # raster visualisation
+library(maptools)
+library(rgeos)
+library(dismo) # allows us to access static maps
+library(rgdal) # goes with dismo
 
 ### Load data ---- 
 # Use the readRDS() function because they are not csv files!
 nwss <- readRDS("data/NWSS.RDS")
+structure <- readRDS("data/species_structure.RDS")
 nnr <- readRDS("data/SNH_national_reserves.RDS")
 
 # Take a look at the data:
@@ -20,7 +28,7 @@ glimpse(nnr)
 unique(nnr$NAME)
 
 # Filter the NWSS dataset so it only contains certain dominant habitat types:
-nwss <- nwss %>% 
+nwss2 <- nwss %>% 
   filter(DOM_HABITA == "Native pinewood"|DOM_HABITA == "Upland birchwood"
          |DOM_HABITA == "Upland mixed ashwood"|DOM_HABITA == "Upland oakwood"
          |DOM_HABITA == "Wet woodland"|DOM_HABITA == "Lowland mixed deciduous woodland")
@@ -32,5 +40,28 @@ nnr2 <- nnr %>%
   mutate(NAME = recode_factor(NAME, "The Great Trossachs Forest" = "Trossachs")) %>% 
   mutate(NAME = recode_factor(NAME, "Mar Lodge Estate" = "Cairngorms")) %>% 
   mutate(NAME = recode_factor(NAME, "Abernethy" = "Cairngorms"))
+
+# Create a map of each of the three areas
+# This did not work (I need package after package, which isn't efficient with my slow internet)
+# scotmap <- gmap("Scotland", type = "satellite", exp = 3)
+# plot(scotmap)
+
+# Another crack at plotting the shape files, this time specifically using the rgdal package
+# Code from this website: https://www.r-graph-gallery.com/168-load-a-shape-file-into-r.html
+map1 <- readOGR( 
+  dsn= paste0(nnr2) , 
+  verbose=FALSE
+)
+plot(nnr2$geometry)
+
+# 3. Calculate the percentage of land covered by each habitat in the 3 areas
+# and display it in a stacked bar graph
+
+# This wasn't in the 'Specific tasks' section but I think I need to merge nwss2 and structure
+habitats3 <- merge(nwss2, structure, by = "SCPTDATA_I", all = FALSE)
+# How do these data correspond to the data in the nnr2 dataframe?
+
+
+
 
 
